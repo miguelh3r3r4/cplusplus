4021bfbd67b3d98e73d9d555c6bcc3f229891be4
William Páez <paez.william8@gmail.com>
2022-01-15 19:55:21 -0400

    Programa de la suma

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, suma;
+    cout << "Ingrese el primer número: ";
+    cin >> a;
+    cout << "Ingrese el segundo número: ";
+    cin >> b;
+    suma = a + b;
+    cout << "La suma es: " << suma << endl;
+    return 0;
+}
